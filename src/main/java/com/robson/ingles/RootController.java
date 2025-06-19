4e1bc8a06079225f951aa29508a272fca3592f1c
Robson Ramos <robson.ramos@finaya.tech>
2025-06-18 21:43:36 -0300

    Adiciona suporte a POST / para validação do Gupshup

--- src/main/java/com/robson/ingles/RootController.java
+++ src/main/java/com/robson/ingles/RootController.java
@@ -1,6 +1,8 @@
 package com.robson.ingles;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,10 @@ public class RootController {
     public String home() {
         return "API de Curso de Inglês ativa!";
     }
+    @PostMapping("/")
+    public ResponseEntity<String> handleGupshupValidation() {
+        return ResponseEntity.ok("POST recebido com sucesso!");
+    }
+
 }
 
